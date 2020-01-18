5a2f60e31a428154216f4b01859819d107131161
DustinG98 <thedustingraham@gmail.com>
2020-01-17 20:07:35 -0700

    Basic structure for hamburger menu, in progress

---
+++ portfolio/src/components/HamburgerMenu/Menu.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HamburgerMenu = () => {
+    return (
+        <div>
+            <div class="bar1"></div>
+            <div class="bar2"></div>
+            <div class="bar3"></div>
+        </div>
+    )
+}
+
+export default HamburgerMenu
