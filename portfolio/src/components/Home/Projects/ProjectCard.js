c9f63da013a611937c4867e8331f7818ff1ce384
DustinG98 <thedustingraham@gmail.com>
2020-01-18 14:58:16 -0700

    ReactJS Popup for projects started.

--- portfolio/src/components/Home/Projects/ProjectCard.js
+++ portfolio/src/components/Home/Projects/ProjectCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Popup from 'reactjs-popup'
 
 import './projects.css'
 
@@ -13,7 +14,15 @@ const ProjectCard = ({ project }) => {
                 {project.tech.map(tech => {
                     return <h4>{tech}</h4>
                 })}
-                <button>Expand</button>
+                <Popup trigger={<button className="expand">Expand</button>}
+                    modal
+                    closeOnDocumentClick
+                >
+                    {close => <div style={{width: '100%', height: '80vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                        <button className="close" style={{ zIndex: '999' }} onClick={close}>&times;</button>
+                        <h1 style={{color: "black"}}>Modal</h1>
+                    </div>}
+                </Popup>
             </div>
         </div>
     )
